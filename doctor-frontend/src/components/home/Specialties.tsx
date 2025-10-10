304077881d8dd2630c0ecd90720902b24a3669f6
Abdul Munim Imran <45959578+munim001@users.noreply.github.com>
2025-10-10 21:19:57 +0500

    specialty icon insertion

--- doctor-frontend/src/components/home/Specialties.tsx
+++ doctor-frontend/src/components/home/Specialties.tsx
@@ -58,6 +58,7 @@ const iconMap: Record<string, any> = {
   "palliative care": Heart,
   "vascular surgery": Droplet,
   "ent surgery": Ear,
+  "Anaesthesiology": Syringe,
 };
 
 export const Specialties = () => {
